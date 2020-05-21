c58b8851bd797ab189c17718577bd5ec727e46c3
Jon <jonathanjackson424@gmail.com>
2020-05-21 16:46:38 -0400

    ...

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,23 +1,6 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-// Here we're adding extra stuff to the "node" (like the slug)
-// so we can query later for all blogs and get their slug
-exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
-  if (node.internal.type === 'MarkdownRemark') {
-    const value = createFilePath({ node, getNode })
-    createNodeField({
-      // Individual MDX node
-      node,
-      // Name of the field you are adding
-      name: 'fields',
-      // Generated value based on filepath with "blog" prefix
-      value: `/blog${value}`
-    })
-  }
-}
-
 exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
